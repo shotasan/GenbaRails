21cd61cf202deeb10f91f9331448782a5d450698
shotasan <wondermoai@gmail.com>
2019-05-13 22:19:18 +0900

    Chapter7-3

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,10 +7,14 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
+  # 検索対象にすることを許可するカラムを指定する
+  # 名称と登録日時のみ許可する
   def self.ransackable_attributes(auth_objet = nil)
     %w[name created_at]
   end
 
+  # 検索条件に含める関連を指定する
+  # 空の配列を返すようにオーバーライドすると、意図しない関連を含めないようにできる
   def self.ransackable_associations(auth_object = nil)
     []
   end
