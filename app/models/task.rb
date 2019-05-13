c2720ef1f9000b713422a9c85af466f338c4c448
shotasan <wondermoai@gmail.com>
2019-05-13 23:37:51 +0900

    Chapter 7-5

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  has_one_attached :image
+
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
