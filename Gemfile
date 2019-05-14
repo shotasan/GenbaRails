aa4d943e8068ad08b6bf94afe3b1594ab2577e17
shotasan <wondermoai@gmail.com>
2019-05-14 12:14:19 +0900

    Chapter 7-7

--- Gemfile
+++ Gemfile
@@ -87,3 +87,5 @@ gem 'ed25519', '>= 1.2', '< 2.0'
 gem 'bcrypt_pbkdf', '>= 1.0', '< 2.0'
 
 gem 'ransack'
+
+gem 'kaminari'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,10 @@
 class Task < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
 
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
-
-  belongs_to :user
 
   scope :recent, -> { order(created_at: :desc) }
 
